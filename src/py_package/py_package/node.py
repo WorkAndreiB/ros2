33793b5db73ddb9bfec99d789425b7ff484edfc2
Andrei B <andrei.baciu94@gmail.com>
2025-06-04 15:37:37 +0000

    Create timer and timer_callback

--- src/py_package/py_package/node.py
+++ src/py_package/py_package/node.py
@@ -6,7 +6,14 @@ from rclpy.node import Node
 class MyNode(Node):
     def __init__(self):
         super().__init__("py_test")
+        self.contor = 0
         self.get_logger().info("Hello world! oop")
+        self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        self.get_logger().info(f"Hello from timer: {self.contor}")
+        self.contor += 1
+    
 
 def main(args=None):
     #first thing is to init
