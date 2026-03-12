f5af0365df9d85ec7839e8d14af0ba493ff938d6
Andrei B <andrei.baciu94@gmail.com>
2026-03-12 17:51:08 +0000

    Fix issue in server code

--- simple_ros_components/actions/action_server_py/action_server_py/add_until_server.py
+++ simple_ros_components/actions/action_server_py/action_server_py/add_until_server.py
@@ -40,7 +40,7 @@ class AddUntilServer(Node):
         super().__init__("add_until_server")
         self.goal_handle_: ServerGoalHandle = None
         self.goal_lock_ = threading.Lock()
-        self.goal_queue_: deque
+        self.goal_queue_: deque = deque()
         self.is_goal_in_progress_ = False
         self.add_until_server_ = ActionServer(
             node=self,
@@ -120,7 +120,6 @@ class AddUntilServer(Node):
         Returns:
             AddUntil.Result: Final result object containing the computed sum.
         """
-        self.set_goal_execution_status(True, goal_handle)
 
         number = goal_handle.request.target_number
         period = goal_handle.request.period
@@ -264,6 +263,7 @@ class AddUntilServer(Node):
                 execute_now = True
 
         if execute_now:
+            self.set_goal_execution_status(True, goal_handle)
             goal_handle.execute()
 
     def cancel_callback(self, goal_handle: ServerGoalHandle):
