4033a491bf1f4b8a62eadb6d4bb1d968746e9994
Andrei B <andrei.baciu94@gmail.com>
2026-04-02 13:24:53 +0000

    Make action client  use command line arguments

--- simple_ros_components/actions/action_client_cpp/src/main.cpp
+++ simple_ros_components/actions/action_client_cpp/src/main.cpp
@@ -4,7 +4,18 @@ int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
 
   auto node = std::make_shared<AddUntilClient>();
-  node->send_goal(10, 0.2);
+  node->declare_parameter<int>("target_number", 10);
+  node->declare_parameter<double>("period", 0.2);
+
+  RCLCPP_INFO(node->get_logger(),
+              "Starting AddUntilClient with target_number=%ld and period=%.2f",
+              node->get_parameter("target_number").as_int(),
+              node->get_parameter("period").as_double());
+
+  const auto target_number = node->get_parameter("target_number").as_int();
+  const auto period = node->get_parameter("period").as_double();
+
+  node->send_goal(target_number, period);
 
   rclcpp::spin(node);
   rclcpp::shutdown();
